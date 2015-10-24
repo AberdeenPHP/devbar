cd53d6c9baeacb7c699c49a775c97d8a46cd9e32
Andy Gaskell <andygaskelluk@yahoo.co.uk>
2015-10-24 18:41:16 +0100

    ok

--- js/main.js
+++ js/main.js
@@ -80,12 +80,21 @@ function initMap() {
 }
 
 function infoContent(marker){
-    return '<div id="content">'+
+    var output = '';
+    output += '<div id="content">'+
         '<div id="siteNotice">'+
         '</div>'+
         '<h1 id="firstHeading" class="firstHeading">'+marker.title+'</h1>'+
         '<div id="bodyContent">'+
-        '<p><b>'+marker.title+'</b> '+marker.description+'</p>'+
-        '</div>'+
-        '</div>';
+        '<p><b>'+marker.title+'</b> '+marker.description+'</p>';
+        
+        if (marker.media != "") {
+            output += '<img src=\'https://rawgit.com/AberdeenPHP/devbar/master/media/'+ marker.media + '\'>'+
+        }
+        if (marker.url != "") {
+            output += '<a href=\''+ marker.url + '\'>read more...</a>'+
+        }        
+    output += '<div id="content">'+'</div>'+
+        '</div>';    
+
 }
