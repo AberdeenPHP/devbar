67fb3c47fe67f45999a9e4d66a632ee5fea220aa
Danny Wilson <danny@iamdannywilson.com>
2015-10-24 13:08:42 +0100

    Base Map implementation and some small style stuff

--- js/main.js
+++ js/main.js
@@ -1,1 +1,57 @@
+function initMap() {
+    var customMapType = new google.maps.StyledMapType([{
+        stylers: [{
+            hue: '#1A9CE3'
+        }, {
+            saturation: 50
+        }, {
+            visibility: 'simplified'
+        }, {
+            gamma: 2
+        }, {
+            weight: 4
+        }]
+    }, {
+        featureType: 'road.local',
+        elementType: 'labels',
+        stylers: [{
+            visibility: 'off'
+        }]
+    },{
+        featureType: 'poi',
+        elementType: 'labels',
+        stylers: [{
+            visibility: 'off'
+        }]
+    }, {
+        featureType: 'water',
+        stylers: [{
+            color: '#1A9CE3',
+        }, {
+            saturation: -50
+        }]
+    }], {
+        name: 'Custom Style'
+    });
+    var customMapTypeId = 'custom_style';
 
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: {
+            lat: 57.1499749,
+            lng: -2.1960765
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_CENTER
+        },
+        streetViewControl:false,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
+        }
+    });
+
+    map.mapTypes.set(customMapTypeId, customMapType);
+    map.setMapTypeId(customMapTypeId);
+
+}
