0fe3cf67adbe621492c499fb476a6f1ce85b9c26
Danny Wilson <danny@iamdannywilson.com>
2015-10-24 14:27:49 +0100

    adding marker placement and infowindow stuff

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ function initMap() {
     var customMapTypeId = 'custom_style';
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 17,
+        zoom: 14,
         center: {
             lat: 57.1499749,
             lng: -2.1960765
@@ -54,4 +54,35 @@ function initMap() {
     map.mapTypes.set(customMapTypeId, customMapType);
     map.setMapTypeId(customMapTypeId);
 
+    // TODO: get js object from CSV
+
+    // markers.map(addMarker())
+
+}
+
+function addMarker(marker, map) {
+    var infowindow = new google.maps.InfoWindow({
+        content: infoContent(marker)
+    });
+
+    var marker = new google.maps.Marker({
+        position: {lat: marker.lat, lng: marker.long},
+        map: map,
+        title: marker.title
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
+
+function infoContent(marker){
+    return '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">'+marker.title+'</h1>'+
+        '<div id="bodyContent">'+
+        '<p><b>'+marker.title+'</b>'+marker.description+'</p>'+
+        '</div>'+
+        '</div>';
 }
